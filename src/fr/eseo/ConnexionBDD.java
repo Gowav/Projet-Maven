bed37cfe799afea8d9126876407233d6a154c94d
Gowav <audouit.fabien@gmail.com>
2019-04-02 09:28:06 +0200

    test commit 3

--- src/fr/eseo/ConnexionBDD.java
+++ src/fr/eseo/ConnexionBDD.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnexionBDD {
-
-	//test
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,9 +17,9 @@ public class ConnexionBDD {
 			
 			java.sql.Statement stat = conn.createStatement();
 			ResultSet res = stat.executeQuery("SELECT * FROM ville_france ");
-			/*ResultSet res = stat.executeQuery("SELECT * FROM ville_france WHERE Code_postal LIKE '49%'");
+			/*ResultSet res = stat.executeQuery("SELECT * FROM ville_france WHERE Code_postal LIKE '49%'");*/
 			System.out.println("Code_commune_INSEE - Nom_commune - Code_postal - Libelle_acheminement - Ligne_5 - Lattitude - Longitude");
-			System.out.println("****************************************************************************************************************");*/
+			System.out.println("****************************************************************************************************************");
 			while(res.next())
 				System.out.println(res.getInt(1)+" - "+res.getString(2)+" - "+res.getInt(3)+" - "+res.getString(4)+" - "+res.getString(5)+" - "+res.getFloat(6)+" - "+res.getFloat(7));
 			
